ac44475c65cac8a4258e28aa765c541da26b288f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-28 20:57:38 +0300

    create store(object) and refactoring

--- src/index.js
+++ src/index.js
@@ -3,35 +3,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText,
-  subscribe,
-} from "./redux/state";
 
 import "./index.css";
 
-import state from "./redux/state";
+import store from "./redux/state";
 
-export const rerenderDom = (state) => {
+const rerenderDom = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
-        addMessage={addMessage}
-        updateNewMessageText={updateNewMessageText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
-rerenderDom(state);
-subscribe(rerenderDom);
+rerenderDom(store.getState());
+store.subscribe(rerenderDom);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,77 +1,80 @@
-let rerenderDom = () => {};
-const state = {
-  profile: {
-    postsData: [
-      { id: 1, likes: 3, text: "post one" },
-      { id: 2, likes: 7, text: "post two" },
-      { id: 3, likes: 42, text: "post three" },
-      { id: 4, likes: 11, text: "post four" },
-      { id: 5, likes: 2, text: "someting strange" },
-      { id: 6, likes: 42, text: "it works!" },
-    ],
-    newPostText: "",
+const store = {
+  _callSubscriber() {
+    console.log("state changed");
   },
-  dialogs: {
-    dialogsData: [
-      { id: 1, name: "Vasya" },
-      { id: 2, name: "Olya" },
-      { id: 3, name: "Dasha" },
-      { id: 4, name: "Kathreen" },
-      { id: 5, name: "Michael" },
-      { id: 6, name: "Alex" },
-    ],
-    messagesData: [
-      { id: 1, text: "Hi!", isYours: false },
-      { id: 2, text: "Whats up", isYours: true },
-      { id: 3, text: "Hello", isYours: false },
-      { id: 4, text: "Some text", isYours: true },
-      { id: 5, text: "New content", isYours: false },
-    ],
-    newMessage: "",
+  _state: {
+    profile: {
+      postsData: [
+        { id: 1, likes: 3, text: "post one" },
+        { id: 2, likes: 7, text: "post two" },
+        { id: 3, likes: 42, text: "post three" },
+        { id: 4, likes: 11, text: "post four" },
+        { id: 5, likes: 2, text: "someting strange" },
+        { id: 6, likes: 42, text: "it works!" },
+      ],
+      newPostText: "",
+    },
+    dialogs: {
+      dialogsData: [
+        { id: 1, name: "Vasya" },
+        { id: 2, name: "Olya" },
+        { id: 3, name: "Dasha" },
+        { id: 4, name: "Kathreen" },
+        { id: 5, name: "Michael" },
+        { id: 6, name: "Alex" },
+      ],
+      messagesData: [
+        { id: 1, text: "Hi!", isYours: false },
+        { id: 2, text: "Whats up", isYours: true },
+        { id: 3, text: "Hello", isYours: false },
+        { id: 4, text: "Some text", isYours: true },
+        { id: 5, text: "New content", isYours: false },
+      ],
+      newMessage: "",
+    },
+    friends: {
+      friendsList: [
+        { id: 1, name: "Ахиллес" },
+        { id: 2, name: "Эзоп" },
+        { id: 3, name: "Плутарх" },
+      ],
+    },
   },
-  friends: {
-    friendsList: [
-      { id: 1, name: "Ахиллес" },
-      { id: 2, name: "Эзоп" },
-      { id: 3, name: "Плутарх" },
-    ],
+  getState() {
+    return this._state;
+  },
+  addPost() {
+    const newPost = {
+      id: 5,
+      likes: 0,
+      text: this._state.profile.newPostText,
+    };
+    this._state.profile.postsData.push(newPost);
+    this._state.profile.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profile.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    const newMessage = {
+      id: 6,
+      text: this._state.dialogs.newMessage,
+      isYours: false,
+    };
+    this._state.dialogs.messagesData.push(newMessage);
+    this._state.dialogs.newMessage = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogs.newMessage = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export const addPost = () => {
-  const newPost = {
-    id: 5,
-    likes: 0,
-    text: state.profile.newPostText,
-  };
-  state.profile.postsData.push(newPost);
-  state.profile.newPostText = "";
-  rerenderDom(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profile.newPostText = newText;
-  rerenderDom(state);
-};
-
-export const addMessage = () => {
-  const newMessage = {
-    id: 6,
-    text: state.dialogs.newMessage,
-    isYours: false,
-  };
-  state.dialogs.messagesData.push(newMessage);
-  state.dialogs.newMessage = "";
-  rerenderDom(state);
-};
-
-export const updateNewMessageText = (newText) => {
-  state.dialogs.newMessage = newText;
-  rerenderDom(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderDom = observer;
-};
-
-export default state;
+window.store = store;
+export default store;
